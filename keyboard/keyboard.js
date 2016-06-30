1a6175addd0c7100df2e5f50594dde2164856dfc
Zura <prosperrriii@gmail.com>
2016-06-30 16:46:35 +0400

    Added keyboard draw logic

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -1,5 +1,34 @@
-var KEYBOARD = document.getElementById("keyboard");
+function init(lang, input, keyboard){
+  var LAYOUT = "layouts/" + lang + ".json";
+  var INPUT = document.getElementById(input);
+  var KEYBOARD = document.getElementById(keyboard);
 
-function init(){
-  console.log("lorem");
+  $.ajax({
+     url: LAYOUT,
+     success: success,
+     error: error
+   });
+
+   function success(data){
+     var common_version = data.common;
+     var shift_version = data.shift;
+
+     drawKeyboard(common_version);
+
+   }
+
+   function error(){
+     console.log("Error");
+   }
+
+   function drawKeyboard(keyboard){
+     keyboard.forEach(function(row, index){
+       for(var col in row){
+         document.getElementById("col-"+ index + col).textContent = row[col];       }
+     });
+   }
+
+
 }
+
+init("layout", "input-div", "keyboard");
